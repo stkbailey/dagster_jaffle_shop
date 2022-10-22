d6b44b97adf53b60f49e9abfd275c0921e4cafca
Stephen Bailey <stkbailey@gmail.com>
2022-10-21 22:31:48 -0400

    Remove pyc files

--- dagster_jaffle_shop/utils/warnings.py
+++
@@ -1,7 +0,0 @@
-"Filter warnings about Dagster experimental configs."
-import warnings
-
-from dagster import ExperimentalWarning
-
-
-warnings.filterwarnings("ignore", category=ExperimentalWarning)
